faeab222022dbbb903bc55ae9cfa12a55d647b65
vivek pal <vivekgpal97@gmail.com>
2022-11-22 12:32:33 +0530

    first commit

---
+++ sub.java
@@ -0,0 +1,12 @@
+/**
+ * sub
+ */
+public class sub {
+
+    public static void main(String[] args) {
+        int a = 30;
+        int b = 20;
+        int c = a-b;
+        System.out.println(a  +" - "+  b + " = " + c );
+    }
+}
